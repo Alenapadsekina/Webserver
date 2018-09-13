fa3cb1860be8f3de8fe55236a2c98b039f56d2f8
Alenapadsekina <alena.padsekina@itechart-group.com>
2018-09-13 10:18:25 +0300

    Add 2 json files
    1) teams
    2) employees

--- server.js
+++ server.js
@@ -1,37 +1,21 @@
 var fs = require("fs");
 var http = require("http");
-// var url = require('url');
+var url = require('url');
 
-var server = http.createServer((req, res) => {
-//	var q = url.parse(req.url, true);
-//	var filename = '.' + q.pathname;
-//	fs.readFile(filename, function(err, data){
-	console.log('request was made: ' + req.url);
-	fs.readFile('index.html', function(err, data){
-		if (err) {
-			res.writeHead(404, {'Content-Type' : 'text/html'});
-			return res.end("404 NOT FOUND");
-		}
-		res.writeHead(200, {'Content-Type' : 'text/html'});
-		res.write(data);
-		return res.end();
-	});
 
+http.createServer(function (req, res) {
+	 res.writeHead(200, {'Content-Type': 'text/html'});
+	 var q = url.parse(req.url, true).query;
+	  var txt = q.team + " " + q.employee;
+
+	  res.end(txt);
 }).listen(8888, () => console.log("Server has started."));
 
-// var employees = JSON.parse(fs.readFileSync('employees.json', 'utf8'));
 
 
 
 
-
-
-/*
-
-fs.readFile('index.html', (err, data) => {
-  if (err) throw err;
-  var arr = data.toString();
-  arr = JSON.parse(arr);
-  console.log(arr);
-});
-*/
+var teams = JSON.parse(fs.readFileSync('teams.json', 'utf8'));
+var employees = JSON.parse(fs.readFileSync('employees.json', 'utf8'));
+console.log(teams);
+console.log(employees);
